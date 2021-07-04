e3bec8ed9a6635a5be508def74ce63daa89f41f5
simsimjae <simsimjae@naver.com>
2021-07-04 14:02:29 +0900

    서버 자동 재시작 확인

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 setInterval(() => {
   axios
-    .get('http://localhost:4000/api/portfolios')
+    .get('http://158.247.220.207:4000/api/portfolios')
     .then((result) => console.log(result.data.length, new Date().getTime()))
     .catch((e) => console.error('에러발생'));
 }, 300);
