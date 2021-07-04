d7ba8ed407da0f487ffe30b29a32f21e299bca1c
simsimjae <simsimjae@naver.com>
2021-07-04 16:29:09 +0900

    TPS100까지 무중단 배포 성공?

--- test.js
+++ test.js
@@ -5,4 +5,5 @@ setInterval(() => {
     .get('http://158.247.220.207:4000/api/portfolios')
     .then((result) => console.log(result.data.length, new Date().getTime()))
     .catch((e) => console.error('에러발생'));
+  // 어디까지 버티나 테스트
 }, 10);
