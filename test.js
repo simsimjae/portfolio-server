f723a51757a67695cb34dc5e128e5cd2ed37cbbd
simsimjae <simsimjae@naver.com>
2021-07-04 16:47:47 +0900

    TPS50 테스트

--- test.js
+++ test.js
@@ -6,4 +6,4 @@ setInterval(() => {
     .then((result) => console.log(result.data.length, new Date().getTime()))
     .catch((e) => console.error('에러발생'));
   // 어디까지 버티나 테스트
-}, 1000 / 30);
+}, 1000 / 50);
