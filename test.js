7a58f2b0dd2af2014c3910edbf2b3afd18212984
simsimjae <simsimjae@naver.com>
2021-07-04 14:40:38 +0900

    0.5초에 한번 리퀘스트 변경

--- test.js
+++ test.js
@@ -5,4 +5,4 @@ setInterval(() => {
     .get('http://158.247.220.207:4000/api/portfolios')
     .then((result) => console.log(result.data.length, new Date().getTime()))
     .catch((e) => console.error('에러발생'));
-}, 100);
+}, 500);
